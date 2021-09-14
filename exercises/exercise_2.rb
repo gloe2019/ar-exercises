93066a1982d54bd47881a90acb429466edd784b5
Gloria Edem <gloedem@gmail.com>
2021-09-14 14:11:21 +0000

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Find stores 1 & 2
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+# Update store 1
+@store1.update(name: "Burberry")
+#confirm change by outputting store1 data
+puts @store1.name
