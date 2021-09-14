187a5e16d49c82eb4e09c227afc57383cac28c53
Gloria Edem <gloedem@gmail.com>
2021-09-14 14:57:54 +0000

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+#Total revenue for all stores
+@total_revenue = Store.sum("annual_revenue")
+puts "Total Revenue: #{@total_revenue}"
+@average_revenue = Store.average("annual_revenue").truncate(2).to_s('F')
+puts "Average Store Revenue: #{@average_revenue}"
+@high_revenue_stores = Store.where("annual_revenue > 1000000")
+puts "Number of stores earning > $1M/year: #{@high_revenue_stores.count}"
