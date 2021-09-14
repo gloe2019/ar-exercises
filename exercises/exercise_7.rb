de7c23d473dada65eb4aa56ea0e34edc8afff31b
Gloria Edem <gloedem@gmail.com>
2021-09-14 16:29:27 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,16 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Your code goes here ..
+puts "Initial store count: #{Store.count}"
+def add_new_store
+  print "Enter a store name: "
+  store_name = gets.chomp
+  new_store = Store.create(name: store_name)
+  new_store.valid?
+  new_store.errors.messages.each do |error|
+    puts error
+  end
+end
+
+add_new_store
